7ac8dbf5d1f700ba8b900925d5f15069581c9535
FelipeGuzmanL <felipeeduardo.guzmanlobos@alumnos.ulagos.cl>
2022-12-31 02:03:26 -0300

    cambios en el footer

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -5,8 +5,8 @@
       Tip 2: you can also add an image using data-image tag
   -->
   <div class="logo">
-    <a href="https://creative-tim.com/" class="simple-text logo-normal">
-      {{ __('Creative Tim') }}
+    <a href="home" class="simple-text logo-normal">
+      {{ __('Planta Externa Telsur') }}
     </a>
   </div>
   <div class="sidebar-wrapper">
